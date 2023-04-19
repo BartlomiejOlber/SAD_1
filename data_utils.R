836eae030364c49718150a29aeec0899c2eeaeed
Turowska <alicjalom@wp.pl>
2023-04-20 00:12:40 +0200

    fix download data

--- data_utils.R
+++ data_utils.R
@@ -1,12 +1,12 @@
 
 download_data_since_2001 <- function(data_dir = "data"){
-  dir.create(data_dir, showWarnings = FALSE)
+  dir.create(data_dir, showWarnings = TRUE)
   for (year in 2001:2022) {
     for (month in 1:2){
       url = sprintf("https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/klimat/%s/%s_0%s_k.zip", year, year, month)
       fpath = sprintf("%s/%s_0%s_k.zip", data_dir, year, month)
       fpath_redundant = sprintf("%s/k_d_t_0%s_%s.csv", data_dir, month, year)
-      if (file.exists(fpath))
+      if (!file.exists(fpath))
         download.file(url, fpath)
       unzip(fpath, exdir=data_dir)
       file.remove(fpath)
@@ -16,13 +16,13 @@ download_data_since_2001 <- function(data_dir = "data"){
 }
 
 download_data_latest <- function(data_dir = "data"){
-  dir.create(data_dir, showWarnings = FALSE)
+  dir.create(data_dir, showWarnings = TRUE)
   for (year in 2022:2023) {
     for (month in 1:2){
       url = sprintf("https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/klimat/%s/%s_0%s_k.zip", year, year, month)
       fpath = sprintf("%s/%s_0%s_k.zip", data_dir, year, month)
       fpath_redundant = sprintf("%s/k_d_t_0%s_%s.csv", data_dir, month, year)
-      if (file.exists(fpath))
+      if (!file.exists(fpath))
         try(download.file(url, fpath), silent=TRUE)
       unzip(fpath, exdir=data_dir)
       file.remove(fpath)
@@ -32,13 +32,13 @@ download_data_latest <- function(data_dir = "data"){
 }
 
 download_data_until_2001 <- function(data_dir = "data"){
-  dir.create(data_dir, showWarnings = FALSE)
+  dir.create(data_dir, showWarnings = TRUE)
   for (year in seq(1951, 1996, 5)) {
     for (year2 in 0:4){
       url = sprintf("https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/klimat/%s_%s/%s_k.zip", year, year + 4, year + year2)
       fpath = sprintf("%s/%s_k.zip", data_dir, year + year2)
       fpath_redundant = sprintf("%s/k_d_t_%s.csv", data_dir, year + year2)
-      if (file.exists(fpath))
+      if (!file.exists(fpath))
         download.file(url, fpath)
       unzip(fpath, exdir=data_dir)
       file.remove(fpath)
